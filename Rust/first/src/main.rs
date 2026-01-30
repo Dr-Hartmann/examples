f4828c2c85d042811cf6f8e788198acb81e7457f
Hartmann <dr.hartmann86@gmail.com>
2026-01-30 11:48:57 +0300

    30.01.2026

--- Rust/first/src/main.rs
+++ Rust/first/src/main.rs
@@ -1,3 +1,15 @@
+mod algo;
+use algo::Train;
+
+mod or_test;
+use or_test::or_test;
+
+mod chrono_test;
+use chrono_test::chrono_test;
+
 fn main() {
-    println!("Hello, world!");
+    let train = Train::new("moscow", 1_000_000, -0b0111_1111);
+    train.hello();
+    or_test();
+    chrono_test();
 }
